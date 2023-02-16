d22c90455983dbdfe6872530516034429bb18dd6
thomas.bertrand <thomas.bertrand@elca.ch>
2023-02-16 09:53:03 +0100

    add carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 function App() {
   return (
     <div className="App">
-      yaay
+      <Carousel
+        transitionTime={400}
+        showArrows={false}
+        showStatus={false}
+        showThumbs={false}
+        interval={4000}
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+      >
+        <div>
+          <img src="assets/01.jpg" />
+        </div>
+        <div>
+          <img src="assets/02.jpg" />
+        </div>
+        <div>
+          <img src="assets/03.jpg" />
+        </div>
+      </Carousel>
     </div>
   );
 }
